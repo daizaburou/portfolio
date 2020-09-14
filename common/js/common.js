2fa6b861b2af3849b2fe8f2d324de951e4461c31
daizaburo <daizabro@gmail.com>
2020-09-14 22:03:15 +0900

    更新

--- common/js/common.js
+++ common/js/common.js
@@ -1,26 +1,28 @@
-let mediaSp = matchMedia('(max-width: 767px)').matches;
-
-$(function(){
-   $('a[href^="#"]').click(function() {
-      let speed = 500; 
-      let href= $(this).attr("href");
-      let target = $(href == "#" || href == "" ? 'html' : href);
-      let position = target.offset().top;
-      $('body,html').animate({scrollTop:position}, speed, 'swing');
-      return false;
+(function ($) {
+   let mediaSp = matchMedia('(max-width: 767px)').matches;
+   $(function(){
+      $('a[href^="#"]').click(function() {
+         let speed = 500; 
+         let href= $(this).attr("href");
+         let target = $(href == "#" || href == "" ? 'html' : href);
+         let position = target.offset().top;
+         $('body,html').animate({scrollTop:position}, speed, 'swing');
+         return false;
+      });
    });
-});
-$(document).ready(function() {
-   if(mediaSp){
-      return false;
-   }
-   let headerHeight = $('header').height();
-   let pagetop = $('#to-top');
-	$(window).scroll(function () {
-		if ($(this).scrollTop() > headerHeight) {
-			pagetop.fadeIn();
-		} else {
-			pagetop.fadeOut();
-		}
-	});
-});
+   $(document).ready(function() {
+      if(mediaSp){
+         return false;
+      }
+      let headerHeight = $('header').height();
+      let pagetop = $('#to-top');
+      $(window).scroll(function () {
+         if ($(this).scrollTop() > headerHeight) {
+            pagetop.fadeIn();
+         } else {
+            pagetop.fadeOut();
+         }
+      });
+   });
+}(jQuery));
+
