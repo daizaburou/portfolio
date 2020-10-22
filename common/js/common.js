ce903078f852632e937ef86e308acc74be56792e
daizaburo <daizabro@gmail.com>
2020-10-22 22:44:23 +0900

    h1のロゴの大きさ調整 アンカー時のハンバーガ開閉

--- common/js/common.js
+++ common/js/common.js
@@ -7,6 +7,13 @@
          let target = $(href == "#" || href == "" ? 'html' : href);
          let position = target.offset().top;
          $('body,html').animate({scrollTop:position}, speed, 'swing');
+         if($('.btn-hamb').hasClass('active')){
+            $('.nav-wrapper').removeClass('active');
+            $('.btn-hamb').removeClass('active');
+         }
+         else{
+            return false;
+         }
          return false;
       });
    });
